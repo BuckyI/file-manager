b6ed7ed6e7cf6eb7859f6bca89fe8e5ad97571f7
mjolnir <mightymjolnir@qq.com>
2023-10-14 20:00:28 +0800

    ✨ feat: 使用类实现文件过滤，增加了缓存进行加速

--- filemanager/scan.py
+++ filemanager/scan.py
@@ -6,27 +6,38 @@ from pathlib import Path
 
 from .datatype import File
 
-EXCLUDE = [
-    ".*",  # hidden folders
-    "__*__",  # __pycache__
-    "*.exe",  # executables
-]
 
+class ScanFilter:
+    EXCLUDE = [
+        ".*",  # hidden folders
+        "__*__",  # __pycache__
+        "*.exe",  # executables
+    ]
 
-def is_valid(path: Path) -> bool:
-    if not path.is_file():
-        return False
-    path = path.absolute()
-    for pattern in EXCLUDE:
+    def __init__(self) -> None:
+        # there exists many same string that is allowed
+        self._cache_permit = set()
+
+    def is_valid(self, path: Path) -> bool:
+        if not path.is_file():  # not an existing file
+            return False
         for part in path.parts:
-            if fnmatch.fnmatch(part, pattern):
-                return False
-    return True
+            if part in self._cache_permit:
+                continue
+            for pattern in self.EXCLUDE:
+                if fnmatch.fnmatch(part, pattern):
+                    return False
+            else:
+                self._cache_permit.add(part)
+        return True
 
 
 def scan_directory(directory: str, *, save: bool = True):
     directory: Path = Path(directory)
-    filedata = [File(path).to_dict() for path in directory.rglob("*") if is_valid(path)]
+    sf = ScanFilter()
+    filedata = [
+        File(path).to_dict() for path in directory.rglob("*") if sf.is_valid(path)
+    ]
     data = {
         "system": platform.platform(),
         "scan_directory": str(directory.absolute()),
