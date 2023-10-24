6bc361013c67829d3b390da1ad51385218f2172d
mjolnir <mightymjolnir@qq.com>
2023-10-24 11:30:57 +0800

    ✨ feat: 增加了颜色输出特性

--- main.py
+++ main.py
@@ -8,6 +8,34 @@ from filemanager.scan import scan_directory
 from filemanager.storage import Database
 
 
+def echo_colored_text(text: str, color: str = "red") -> str:
+    """
+    red: error
+    yellow: warning
+    blue: info
+    green: success
+    """
+    match color:
+        case "red":
+            click.echo("\033[31m" + text + "\033[0m")
+        case "green":
+            click.echo("\033[32m" + text + "\033[0m")
+        case "yellow":
+            click.echo("\033[33m" + text + "\033[0m")
+        case "blue":
+            click.echo("\033[34m" + text + "\033[0m")
+        case _:
+            click.echo(text)
+
+
+@click.command()
+def debug():
+    echo_colored_text("Welcome! ", "red")
+    echo_colored_text("Welcome! ", "green")
+    echo_colored_text("Welcome! ", "yellow")
+    echo_colored_text("Welcome! ", "blue")
+
+
 @click.group(help="scan file information")
 def cli():
     pass
@@ -30,17 +58,17 @@ def cli():
 )
 def scan(directory, database):
     if not Path(directory).exists():
-        click.echo("directory does not exist!")
+        echo_colored_text("directory does not exist!")
         return
     scan_directory(directory, save=True, db_path=database)
-    click.echo(f"Scan {directory} finished")
+    echo_colored_text(f"Scan {directory} finished", "green")
 
 
 @click.command()
 @click.argument("database", type=click.Path())
 def init_database(database):
     db = Database(database)
-    click.echo("Initialized the database")
+    echo_colored_text("Initialized the database", "green")
 
 
 @click.command()
@@ -58,7 +86,7 @@ def submit(database, source):
     if source.is_dir():
         files = list(source.rglob("filescan_*.json"))
         if not len(files):
-            click.echo("no scan file found!")
+            echo_colored_text("no scan file found!")
             return
     else:
         files = [source]
@@ -68,10 +96,10 @@ def submit(database, source):
         try:
             data = json.load(open(file, "r", encoding="utf-8"))
             db.update_from_dicts(data["files"])
-            click.echo(f"Submit {str(file)} finished")
+            echo_colored_text(f"Submit {str(file)} finished", "green")
         except Exception as e:
-            click.echo(f"Submit {str(file)} failed! Exception: {e}")
-    click.echo("Finished")
+            echo_colored_text(f"Submit {str(file)} failed! Exception: {e}")
+    echo_colored_text("Finished", "green")
 
 
 cli.add_command(scan)
